d7f4ec446c97109b6f6cfab0b94fa3c6f49796fe
unknown <conormcgeary04@gmail.com>
2026-02-20 00:50:50 +0000

    Add Models and Views

--- controllers/about.js
+++ controllers/about.js
@@ -1,11 +1,16 @@
 'use strict';
 
+import logger from '../utils/logger.js';
+import appStore from '../models/app-store.js';
+import employeeStore from '../models/employee.js';
+
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
-    response.send('About the Playlist app');   
+    const appInfo = appStore.getAppInfo();
+    const employee = employeeStore.getAppInfo();
+    response.render('about', { title: 'About', appInfo: appInfo, employee: employee });
   },
 };
-
 
 export default about;
